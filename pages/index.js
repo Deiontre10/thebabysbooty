effbd996636f8372a3737153fe45887bf72dfb0c
Taz Lowry <travontazlowry@gmail.com>
2023-04-18 22:22:18 -0400

    added auth

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
-
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
+  const { data: session } = useSession();
+
+  if(!session) {
+    return (
+      <div className="bg-purple-500 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
+        </div>
+      </div>
+    )
+  }
   return (
-    <h1 className="bg-red-500">Test</h1>
+    <div className="">Logged in {session.user.email}</div>
   )
 }
