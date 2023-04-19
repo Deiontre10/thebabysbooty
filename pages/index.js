722c0ab1a3ec117f036350b34b5489b748fbf2ea
Taz Lowry <travontazlowry@gmail.com>
2023-04-19 16:57:53 -0400

    setup Layout.js as a component

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,7 @@
-import { useSession, signIn, signOut } from "next-auth/react";
-import Nav from "../components/Nav";
+import Layout from "@/components/Layout";
 
 export default function Home() {
-  const { data: session } = useSession();
-
-  if (!session) {
-    return (
-      <div className="bg-purple-500 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
-        </div>
-      </div>
-    )
-  }
-  return (
-    <div className="bg-purple-500 min-h-screen">
-      <Nav />
-      <div className="">Logged in {session.user.email}</div>
-    </div>
-  )
+  return <Layout>
+    test
+  </Layout>;
 }
