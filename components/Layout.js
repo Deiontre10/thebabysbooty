0bd245c171d9560f1b22b749fe09c0bdc105e87c
Taz Lowry <travontazlowry@gmail.com>
2023-04-19 17:19:13 -0400

    Reconfigured the nav, index and layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,12 @@
 import { useSession, signIn, signOut } from "next-auth/react";
-import Nav from "../components/Nav";
+import Nav from "@/components/Nav";
 
 export default function Layout({children}) {
   const { data: session } = useSession();
 
   if (!session) {
     return (
-      <div className="bg-purple-500 w-screen h-screen flex items-center">
+      <div className="bg-purple-600 w-screen h-screen flex items-center">
         <div className="text-center w-full">
           <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
         </div>
@@ -14,7 +14,7 @@ export default function Layout({children}) {
     )
   }
   return (
-    <div className="bg-purple-500 min-h-screen flex">
+    <div className="bg-purple-600 min-h-screen flex">
       <Nav />
       <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">{children}</div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
+
 
 export default function Home() {
+  const { data: session } = useSession();
+
   return <Layout>
-    test
+    <div className="text-purple-600">
+      Hello, <b>{session?.user?.name}</b>
+    </div>
   </Layout>;
 }
