a74743dfc7a5c296e1409a637e7822ffdd6b4e9b
Taz Lowry <travontazlowry@gmail.com>
2023-04-23 20:30:54 -0400

    updated new.js with state

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,17 +1,44 @@
 import Layout from '@/components/Layout';
-
+import { useState } from 'react';
 
 export default function NewProduct() {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState(0);
+
+    async function createProduct() {
+        const data = { title, description, price };
+        await axios.post('/api/products', data)
+    }
+
     return (
         <Layout>
-            <h1>New Product</h1>
-            <label>Product Name</label>
-            <input type="text" placeholder="Product name" />
-            <label>Product Description</label>
-            <textarea placeholder="Product description" />
-            <label>Price</label>
-            <input type="number" placeholder="Price" />  
-            <button className='btn-primary'>Save</button>
+            <form onSubmit={ createProduct }>
+                <h1>New Product</h1>
+                <label>Product Name</label>
+                <input
+                    type="text"
+                    placeholder="Product name"
+                    value={title}
+                    onChange={ev => setTitle(ev.target.value)} />
+                <label>Product Description</label>
+                <textarea
+                    placeholder="Product description"
+                    value={description}
+                    onChange={ev => setDescription(ev.target.value)} />
+                <label>Price</label>
+                <input
+                    type="number"
+                    placeholder="Price"
+                    value={price}
+                    onChange={ev => setPrice(ev.target.value)} />
+                <button
+                    type='sumbit'
+                    className='btn-primary'>
+                    Save
+                </button>
+
+            </form>
         </Layout>
     );
 }
